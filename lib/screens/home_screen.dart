59d61018a7fe03d345d7b7ec70a04893fc02e798
Zoholar <qudus.shokunbi@gmail.com>
2023-02-14 15:02:54 +0100

    dash board layout complete

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,6 +10,97 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Card makeDashboardItem(String title, IconData iconData, int index) {
+    return Card(
+      elevation: 2,
+      margin: EdgeInsets.all(10),
+      child: Container(
+        decoration: index == 0 || index == 3 || index == 4
+            ? const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.cyan,
+                    Colors.amber,
+                  ],
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp,
+                ),
+              )
+            : const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [
+                    Colors.redAccent,
+                    Colors.amber,
+                  ],
+                  begin: FractionalOffset(0.0, 0.0),
+                  end: FractionalOffset(1.0, 0.0),
+                  stops: [0.0, 1.0],
+                  tileMode: TileMode.clamp,
+                ),
+              ),
+        child: InkWell(
+          onTap: () {
+            if (index == 0) {
+              // new available order
+
+            }
+            if (index == 1) {
+              // perc
+            }
+            if (index == 2) {
+              // new order
+            }
+            if (index == 3) {
+              // history
+            }
+            if (index == 4) {
+              // total earnings
+            }
+            if (index == 5) {
+              // log out
+
+              firebaseAuth.signOut().then((value) {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const AuthScreen(),
+                  ),
+                );
+              });
+            }
+          },
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisSize: MainAxisSize.min,
+            verticalDirection: VerticalDirection.down,
+            children: [
+              const SizedBox(height: 50.0),
+              Center(
+                child: Icon(
+                  iconData,
+                  size: 40,
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Center(
+                child: Text(
+                  title,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    color: Colors.black,
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,25 +120,29 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         title: Text(
-          sharedPreferences!.getString('name')!,
+          'Welcome ' + sharedPreferences!.getString('name')!,
+          style: const TextStyle(
+              fontSize: 25,
+              color: Colors.black,
+              fontFamily: 'Signatra',
+              letterSpacing: 2),
         ),
         centerTitle: true,
         automaticallyImplyLeading: false,
       ),
-      body: Center(
-        child: ElevatedButton(
-          style: ElevatedButton.styleFrom(backgroundColor: Colors.cyan),
-          onPressed: (() {
-            firebaseAuth.signOut().then((value) => {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (c) => const AuthScreen(),
-                    ),
-                  )
-                });
-          }),
-          child: const Text('Logout'),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 1),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: const EdgeInsets.all(2),
+          children: [
+            makeDashboardItem('New Available Orders', Icons.assessment, 0),
+            makeDashboardItem('Parcel in Progress', Icons.airport_shuttle, 1),
+            makeDashboardItem('Not Yet Delivered', Icons.location_history, 2),
+            makeDashboardItem('History', Icons.done_all, 3),
+            makeDashboardItem('Total Earning', Icons.monetization_on, 4),
+            makeDashboardItem('Logout', Icons.logout, 5),
+          ],
         ),
       ),
     );
